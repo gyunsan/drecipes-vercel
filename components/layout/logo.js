a2a99f07405e99c7d3ee6d2145c2d929ee7a9091
Gyuner Saliev <g.saliev@gmail.com>
2023-02-26 20:29:49 +0200

    added link to the logo

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,8 +1,9 @@
+import { Link } from '@mui/material';
 import GreenHexaLogo from '../../public/drecipes-logo-green-hexagon.svg';
 import classes from './logo.module.css';
 
 function Logo() {
-  return <div className={classes.logo}>drecipes</div>;
+  return <div className={classes.logo}><Link href='/'>drecipes </Link></div>;
 }
 
 export default Logo;
